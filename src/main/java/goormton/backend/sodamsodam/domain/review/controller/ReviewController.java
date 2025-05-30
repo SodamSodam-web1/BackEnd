86088479ebbcbc11d026cfa45295127dd72ee294
whoneykkk <whoneykim@gmail.com>
2025-05-30 23:12:52 +0900

    chore(review): 사용하지 않는 import 제거

--- src/main/java/goormton/backend/sodamsodam/domain/review/controller/ReviewController.java
+++ src/main/java/goormton/backend/sodamsodam/domain/review/controller/ReviewController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
