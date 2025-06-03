cfad3bf25c980c610505eafc68edd2acf6d5f02d
T-ferret <hs381717@gmail.com>
2025-06-03 22:39:14 +0900

    북마크 추가 삭제 기능 구현

--- src/main/java/goormton/backend/sodamsodam/global/payload/ErrorCode.java
+++ src/main/java/goormton/backend/sodamsodam/global/payload/ErrorCode.java
@@ -26,6 +26,10 @@ public enum ErrorCode {
     DUPLICATE_REVIEW_TAGS(HttpStatus.BAD_REQUEST, null , "리뷰 태그에 중복된 항목이 존재합니다."),
     FORBIDDEN_REVIEW_DELETE(HttpStatus.FORBIDDEN, null, "본인의 리뷰만 삭제할 수 있습니다."),
 
+    // Bookmark Error
+    BOOKMARK_ALREADY_EXISTS_ERROR(HttpStatus.CONFLICT, null, "이미 북마크된 장소입니다."),
+    BOOKMARK_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, null , "해당 장소를 북마크하지 않았습니다."),
+
     // JWT 토큰
     JWT_EXPIRED_ERROR(HttpStatus.BAD_REQUEST, null, "JWT 토큰이 만료되었습니다."),
     ;
