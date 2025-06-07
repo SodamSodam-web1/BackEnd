fc41b402d8cf132a188a3c582a73f14e42f01a63
T-ferret <hs381717@gmail.com>
2025-06-07 23:31:28 +0900

    getJwt 함수가 String을 받도록, validateToken가 각 상황에 맞는 exception을 throw하도록 변경

--- src/main/java/goormton/backend/sodamsodam/global/payload/ErrorCode.java
+++ src/main/java/goormton/backend/sodamsodam/global/payload/ErrorCode.java
@@ -32,6 +32,7 @@ public enum ErrorCode {
 
     // JWT 토큰
     JWT_EXPIRED_ERROR(HttpStatus.BAD_REQUEST, null, "JWT 토큰이 만료되었습니다."),
+    INVALID_JWT_ERROR(HttpStatus.BAD_REQUEST, null, "유효하지 않은 JWT 표현입니다.")
     ;
 
     private final HttpStatus httpStatus;
